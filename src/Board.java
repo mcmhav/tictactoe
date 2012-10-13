b24e2d4f73c542c31c9380929b75df6f9cb89a15
Martin Havig <mhavig@cisco.com>
2012-10-13 23:53:20 +0200

    first commit

---
+++ src/Board.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: martin
+ * Date: 9/6/12
+ * Time: 5:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Board {
+
+    private Boolean board[][] = new Boolean[3][3];
+}
